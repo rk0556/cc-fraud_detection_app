6e207d0d34a8a4f685660ba20ca691f8c6107a6a
rk0556 <rohitkatoch2711@gmail.com>
2024-09-04 01:42:47 -0400

    Update cc_fraud_detection.py

--- cc_fraud_detection.py
+++ cc_fraud_detection.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from sklearn.ensemble import IsolationForest
 from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA  
 import matplotlib.pyplot as plt
 import seaborn as sns
 
